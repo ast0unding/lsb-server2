e40736cfac70a5d99b0af521dd0846e07841871d
unknown <zlatirl1@gmail.com>
2026-06-19 11:34:51 +0100

    B finish implementing old migrations

--- sql/mob_pools.sql
+++ sql/mob_pools.sql
@@ -1456,6 +1456,10 @@ INSERT INTO `mob_pools` VALUES (8167,'Clavauert_B_Chanoix','Clavauert B Chanoix'
 INSERT INTO `mob_pools` VALUES (9200,'Deviate_Bhoot_NM','Deviate_Bhoot',52,0x0000710100000000000000000000000000000000,4,4,12,240,100,0,1,0,1,2,0,0,0,133,0,0,28,0,0,52,52,2,6);
 INSERT INTO `mob_pools` VALUES (9201,'Psycheflayer_NM','Psycheflayer',233,0x0000EF0600000000000000000000000000000000,4,4,7,240,100,0,1,1,0,2,0,0,0,133,12,0,28,0,0,233,233,2,15);
 
+-- Arrapago Remnants
+INSERT INTO `mob_pools` VALUES (9200,'Deviate_Bhoot_NM','Deviate_Bhoot',52,0x0000710100000000000000000000000000000000,4,4,12,240,100,0,1,0,1,2,0,0,0,133,0,0,28,0,0,52,52,2,6);
+INSERT INTO `mob_pools` VALUES (9201,'Psycheflayer_NM','Psycheflayer',233,0x0000EF0600000000000000000000000000000000,4,4,7,240,100,0,1,1,0,2,0,0,0,133,12,0,28,0,0,233,233,2,15);
+
 -- ------------------------------------------------------------
 -- Start of Ambuscade section
 -- NOTE: The mobs are changed every update in the DATs, so using out-of-date
